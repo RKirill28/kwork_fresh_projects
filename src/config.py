a49885e73d55d21bdedb49f08b74a5173d562dfa
Roca12341241234 <pav.konst.2006@gmail.com>
2025-08-02 11:08:50 +0700

    Added config based on pydantic_settings.

---
+++ src/config.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BotConfig(BaseSettings):
+	token: str
+
+class ApiConfig(BaseSettings):
+    url: str
+
+class FileConfig(BaseSettings):
+    projects_path: Path
+    cats_path: Path
+
+class Settings(BaseSettings):
+    bot_config: BotConfig
+    api_config: ApiConfig
+    file_config: FileConfig
+
+    model_config = SettingsConfigDict(
+    env_file=(
+	    '.env.template',
+	    '.env',
+    ),
+    env_nested_delimiter='__',
+    )
+
+
+settings = Settings()
+
+if __name__ == '__main__':
+    print(settings.bot_config.token)
